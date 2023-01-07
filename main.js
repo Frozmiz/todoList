bc1133bbd59dfbd42a234b5510da1f9cf003d985
Alejandro <frozmiz@live.com>
2023-01-07 23:57:52 +0100

    ref

---
+++ main.js
@@ -0,0 +1,77 @@
+// FUNCION ONLOAD > Asigna los eventos al cargar el documento y declara las variables que se usarán en el código.
+
+window.onload = init;
+
+function init() {
+
+    // VARIABLES DE ÁMBITO GLOBAL
+
+    botonEnvio = document.querySelector('.btn-add');
+    nuevoItem = document.querySelector('[type="text"]');
+    listaCompra = document.getElementById("listaCompra");
+    empty = document.querySelector(".empty"); 
+
+    console.log("ONLOAD");
+    botonEnvio.addEventListener("click", add);
+    
+
+    
+}
+
+// FUNCION ANADIR > Si el cajetín de texto está vacio la función añadir no se ejecuta gracias a preventDefault();
+
+function add(evento){
+
+    console.log("Abre funcion add");
+
+    if (nuevoItem.value == "") {
+        evento.preventDefault();
+    } else {
+        const lista = document.createElement("li");
+        //lista es el elemento padre
+        lista.innerHTML = nuevoItem.value;
+        lista.addEventListener("click", eliminarLi);
+        listaCompra.appendChild(lista);
+        //Agregamos el botón por cada LI que se crea.
+        lista.appendChild(addDeleteButton());
+        //Cada vez que demos al botor añadir el input se pondrá en blanco.
+        nuevoItem.value = "";
+
+        empty.style.display = "none";
+        
+        
+    }
+}
+
+//BOTON ELIMINAR
+
+function addDeleteButton() {
+    const deleteButton = document.createElement("button");
+
+    deleteButton.textContent = "X";
+
+    deleteButton.className = "btn-delete";
+
+    deleteButton.addEventListener("click", (e) => {
+        // creamos variable item y asignamos el elemento padre del evento.
+        const item = e.target.parentElement;
+        //Eliminamos desde el elemento padre el elemento hijo
+        ul.removeChild(item);
+
+        const items = document.querySelectorAll("li")
+
+        if (items.length === 0) {
+            empty.style.display = "block";
+        }
+    });
+
+    return deleteButton;
+}
+
+// FUNCION ELIMINAR LI > Debemos llamar al elemento padre y desde este eliminar al hijo.
+
+function eliminarLi() {
+    this.parentNode.removeChild(this);
+    console.log("Elimina elemento");
+}
+
